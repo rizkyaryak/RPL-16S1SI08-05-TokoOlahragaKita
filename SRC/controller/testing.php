afdac8d1e6ac4bfac4c65eb1055acd675352d4d6
rizkyaryak <rizky.9488@students.amikom.ac.id>
2018-11-30 16:24:02 +0700

    testing

---
+++ SRC/controller/testing.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Belajar extends CI_Controller {
+	
+	function __construct(){
+		parent::__construct();
+		
+	}
+
+	public function index(){
+		echo "Testing";
+	}
+
+	public function halo(){
+		echo "Testing";
+	}
+
+}
+?>
